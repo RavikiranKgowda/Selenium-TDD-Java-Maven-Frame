7303592284c77f9403feff940229993a799fffcd
RavikiranKgowda <ravikiran.k.gowda@gmail.com>
2023-01-29 15:55:33 -0500

    Added, compilers to pom.xml

--- MyDemoProjectFramework/src/test/java/com/framework/pages/BaseClass.java
+++ MyDemoProjectFramework/src/test/java/com/framework/pages/BaseClass.java
@@ -7,6 +7,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -36,11 +37,13 @@ public class BaseClass {
 		Reporter.log("Settings done", true);
 		//Added comment
 	}
-
+	
+	
 	@BeforeClass
 	public void setup() {
 		
 		Reporter.log("Launching browser - "+config.getBrowser()+" and and URL - "+config.getTestingUrl(), true);
+//		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getTestingUrl());
 		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getTestingUrl());
 
 	}
